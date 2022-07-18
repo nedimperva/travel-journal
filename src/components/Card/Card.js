30d19ca5478cd840d52908e7ef62cbd03752c97f
Nedim Perva <nedim.perva@gmail.com>
2022-07-19 00:31:17 +0200

    made site responsive

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,13 +13,15 @@ function Card({
   return (
     <div className="card">
       <div className="card__image">
-        <img src={imageUrl} width={200} height={300} />
+        <img src={imageUrl} alt={title} />
       </div>
       <div className="card__content">
         <div className="content--location">
           <img src={locationIcon} alt="Location Icon" width={15} height={20} />
           <p>{location}</p>
-          <a href={googleMapsUrl}>View on Google Maps</a>
+          <a href={googleMapsUrl} target="_blank">
+            View on Google Maps
+          </a>
         </div>
         <h1 className="content--title">{title}</h1>
         <div className="content--date">
